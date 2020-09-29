8c89f74bde70f8e013b7b3bf5aeab50156cf637f
picolloo <lucaspicolloo@gmail.com>
2020-09-28 22:05:53 -0300

    feat: add pbkdf2

---
+++ projetoPBKDF2/index.js
@@ -0,0 +1,14 @@
+const crypto = require("crypto");
+
+var config = {
+  // size of the generated hash
+  hashBytes: 32,
+  // larger salt means hashed passwords are more resistant to rainbow table, but
+  // you get diminishing returns pretty fast
+  saltBytes: 16,
+  // more iterations means an attacker has to take longer to brute force an
+  // individual password, so larger is better. however, larger also means longer
+  // to hash the password. tune so that hashing the password takes about a
+  // second
+  iterations: 872791,
+};
